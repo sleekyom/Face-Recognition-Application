8bb6ad58ee4267844bcd6edfe0443a922ecdbb26
Emmanuel <amdas72@gmail.com>
2020-03-27 04:46:03 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Naviagtion from "./components/Navigation/Navigation"
+import Logo from "./components/Logo/Logo"
+import ImageLinkForm from "./components/ImageLinkForm"
+import FaceRecognition from "./components/FaceRecognition"
+import 'tachyons'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Naviagtion />
+       <Logo />
+      {/*<ImageLinkForm />
+      <FaceRecognition /> */}
     </div>
   );
 }
