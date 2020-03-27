d09f68f69465511e1d02f87747a207fe67249b8e
Emmanuel <amdas72@gmail.com>
2020-03-27 23:57:46 +0200

    Added Rank, Logo, ImageLinkform and Particle Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from "react";
 import "./App.css";
-import Naviagtion from "./components/Navigation/Navigation"
-import Logo from "./components/Logo/Logo"
-import ImageLinkForm from "./components/ImageLinkForm"
-import FaceRecognition from "./components/FaceRecognition"
-import 'tachyons'
+import Naviagtion from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition";
+import Particles from "react-particles-js";
+import "tachyons";
 
+const particleOptions = {
+  particles: {
+    number:{
+      value:75,
+      density:{
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleOptions} />
       <Naviagtion />
-       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition /> */}
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition /> */}
     </div>
   );
 }
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="f3 white">{`Bukola, your current rank is...`}</div>
+      <div className="f1 white">{`#4`}</div>
+    </div>
+  );
+};
+
+export default Rank;
