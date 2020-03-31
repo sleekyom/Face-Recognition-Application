24c2f31731086f289788a40cd76bdc1f4bafef2f
Emmanuel <amdas72@gmail.com>
2020-03-31 04:45:01 +0300

    Added more components and Bounding-box

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/SignIn/Signin";
 import Particles from "react-particles-js";
 import particleOptions from "../src/particlesOption";
 import "tachyons";
@@ -19,43 +20,67 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {},
+      route: "signin"
     };
   }
 
   onChangeInput = event => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
+  };
+
+  detectFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById("inputimage");
+
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  drawFaceBox = box => {
+    this.setState({ box: box });
   };
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
     app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,this.state.input
-      )
-      .then(
-        function(response) {
-          // do something with response
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function(err) {
-          // there was an error
-        }
-      );
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response => this.drawFaceBox(this.detectFaceLocation(response)))
+      .catch(err => console.log(err));
   };
 
   render() {
     return (
       <div className="App">
         <Particles className="particles" params={particleOptions} />
-        <Naviagtion />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onChangeInput={this.onChangeInput}
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+
+        {this.state.route === "signin" ? (
+          <Signin />
+        ) : (
+          <div>
+            <Naviagtion />
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onChangeInput={this.onChangeInput}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        )}
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,26 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div>
-      <div className="center">
-        <div className="absolute mt3">
-          <img src={imageUrl} alt="" width="300px" height="auto" />
-        </div>
+    <div className="center">
+      <div className="absolute mt3">
+        <img
+          id="inputimage"
+          src={imageUrl}
+          alt=""
+          width="300px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        ></div>
       </div>
     </div>
   );
