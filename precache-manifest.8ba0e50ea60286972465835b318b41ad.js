0f8d878cd208cf9d5e63f8ac30fc30f219ad52cb
Emmanuel <amdas72@gmail.com>
2020-03-31 16:56:06 +0300

    Updates

---
+++ precache-manifest.8ba0e50ea60286972465835b318b41ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86222a1af57fe5ed4c7b461a1b1f0b4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eafe46e6a0114e696a60",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6dfeb8ead45eba5290b9",
+    "url": "/static/css/main.2bb95b92.chunk.css"
+  },
+  {
+    "revision": "eafe46e6a0114e696a60",
+    "url": "/static/js/2.dce1aa4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dce1aa4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dfeb8ead45eba5290b9",
+    "url": "/static/js/main.0f3c7fcc.chunk.js"
+  },
+  {
+    "revision": "b8b1e4a8e93be08133f5",
+    "url": "/static/js/runtime-main.41bade79.js"
+  }
+]);
