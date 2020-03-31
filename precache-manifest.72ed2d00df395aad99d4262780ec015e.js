f6abdafab4f54645f871a0afd3f344164e4fc496
Emmanuel <amdas72@gmail.com>
2020-03-31 17:00:05 +0300

    Updates

---
+++ precache-manifest.72ed2d00df395aad99d4262780ec015e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0035860e539092d7eb0e41e5a0dcca66",
+    "url": "/Face-Recognition-Application/index.html"
+  },
+  {
+    "revision": "b4ee325952037c9d4de1",
+    "url": "/Face-Recognition-Application/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c99fdba6a3ab32b9ef03",
+    "url": "/Face-Recognition-Application/static/css/main.2bb95b92.chunk.css"
+  },
+  {
+    "revision": "b4ee325952037c9d4de1",
+    "url": "/Face-Recognition-Application/static/js/2.01649fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Recognition-Application/static/js/2.01649fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99fdba6a3ab32b9ef03",
+    "url": "/Face-Recognition-Application/static/js/main.73a50de9.chunk.js"
+  },
+  {
+    "revision": "af37ac174f1485512d3c",
+    "url": "/Face-Recognition-Application/static/js/runtime-main.67711c7d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ba0e50ea60286972465835b318b41ad.js"
+  "/Face-Recognition-Application/precache-manifest.72ed2d00df395aad99d4262780ec015e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Face-Recognition-Application/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
